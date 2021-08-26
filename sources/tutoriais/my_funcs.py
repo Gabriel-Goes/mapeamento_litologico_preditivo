9e9ed18e7bd6a5e46fee8bb763011e31214c7aba
Gabriel-Goes <gabrielgoes@usp.br>
2021-08-26 12:57:55 -0300

    automação finalizada

--- sources/tutoriais/my_funcs.py
+++ sources/tutoriais/my_funcs.py
@@ -1,7 +1,13 @@
-import geopandas as gpd
 import math
+import numpy as np
+import verde as vd
+
+import geopandas as gpd
+import pandas as pd
+
 
 gdb = '/home/ggrl/geodatabase/'
+
 
 
 # Importador de Litologias por escala
@@ -28,18 +34,15 @@ def ocrr(substancia):
     subs= ocorrencias
 '''
 
-
-
-
-
 # Nomeador de Grids
 p1kk=['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
 p500k=[['V','Y'],['X','Z']]
 p250k=[['A','C'],['B','D']]
 p100k=[['I','IV'],['II','V'],['III','VI']]
 p50k=[['1','3'],['2','4']]
+p25k=[['NW','SW'],['NE','SE']]
 
-def nomeador_grid(left,right,top,bottom,escala=4):
+def nomeador_grid(left,right,top,bottom,escala=5):
     if left>right:
         print('Oeste deve ser menor que leste')
     if top<bottom:
@@ -76,27 +79,92 @@ def nomeador_grid(left,right,top,bottom,escala=4):
             LO=(math.ceil(right/0.5)%3)-1
             NS=math.ceil(top/0.5)%2!=north
             folha+='_'+p100k[LO][NS]
-        #p50k-----------------------
+        #p50k------------------------
         if (lat_gap<=0.25) & (escala>=4):
             LO=math.ceil(right/0.25)%2==0
             NS=math.ceil(top/0.25)%2!=north
             folha+='_'+p50k[LO][NS]
+        #p25k------------------------
+        if (lat_gap<=0.125) & (escala>=5):
+            LO=math.ceil(right/0.125)%2==0
+            NS=math.ceil(top/0.125)%2!=north
+            folha+='_'+p25k[LO][NS]
         return folha
 
 
 
 
 # Selecionador de Região
-    # Selecionar SF23 Folha Rio de Janeiro Escala 1:1.000.000
-    # Selecionar Intersecção do aerolevantamento 1039
+# Selecionar SF23 Folha Rio de Janeiro Escala 1:1.000.000
+# Selecionar Intersecção do aerolevantamento 1039
+# Definindo Regions (W,E,S,N)
 
+def regions(gdf):
+    bounds = gdf.bounds 
+    gdf['region'] = [(left,right,bottom,top) for left,right,bottom,top in zip(bounds['minx'],bounds['maxx'],
+                                                                                bounds['miny'],bounds['maxy'])]
 
-'''
-def get_region(region,escala):
+# Definindo nomes da malha a partir da articulação sistematica de folhas de cartas. Construindo uma lista e definindo como uma series.
+def nomeador_malha(gdf):
+    df = pd.DataFrame(gdf)
+    lista_grid = []
+    for index, row in df.iterrows():
+        row['id_folha'] = (nomeador_grid(row.region[0],row.region[1],
+                                         row.region[3],row.region[2],escala=5))
+        lista_grid.append(row.id_folha)
 
-    region = [-47.00, -46.75,
-            -22.75, -22.50]
+    gdf['id_folha'] = lista_grid
+##
 
-    'id_geof'+'id_folha' = geof_1039[vd.inside((geof_1039.LONG, geof_1039.LAT), region = region)]
+def select_area(escala,id):
+    malha_cartog = importar('malha_cartog_'+escala+'_wgs84')
+    regions(malha_cartog)
+    df=[]
+    for i in id:
+        print(i)
+        df.append(pd.DataFrame(malha_cartog[malha_cartog['id_folha'].str.contains(i)]))
+    return(df)
 
-'''
+# Iterando entre as regions da malha cartográfica
+def interpolar(escala,id,geof,degree=1,spacing=499,psize=100,n_splits=15,validate=False):
+    chain_list = input('Liste canais a serem interpolados:')      # Lista de canais a serem interpolados
+                                                      
+    grids = {chain_list[0]:(),chain_list[1]:(),chain_list[2]:(),chain_list[3]:(), chain_list[4]:()}    # Dicionário para salvar os dados interpolados (grids)
+    scores = {chain_list[0]:(),chain_list[1]:(),chain_list[2]:(), chain_list[3]:(), chain_list[4]:()}   # Dicionário para salvar os dados da validação cruzada
+    
+    df = select_area(escala,id)
+       
+    for index, row in df.iterrows():
+        print(row.id_folha+' start')
+        data = geof[vd.inside((geof.LONGITUDE, geof.LATITUDE), region = row.region)]
+        coordinates = (data.X.values, data.Y.values)
+        if data.empty == True:  # if data dont have values pass to next step
+            print('none')
+        if len(data) < 2000:    # if less then 2000 points pass to next step
+            print('few data')
+        else:
+            # Iterando entre os canais de interpolação
+            for i in chain_list:
+                print(i)
+                chain = vd.Chain([
+                    ('trend', vd.Trend(degree=degree)),
+                    ('reduce', vd.BlockReduce(np.median, spacing=spacing)),
+                    ('spline', vd.Spline())
+                ])
+                
+                chain.fit(coordinates, data[i])
+                
+                if validate == True:
+                    cv     = vd.BlockKFold(spacing=spacing,
+                                n_splits=n_splits,
+                                shuffle=True)
+
+                    scores[i] = vd.cross_val_score(chain,
+                                            coordinates,
+                                            data[i],
+                                            cv=cv)
+                    
+                grid = chain.grid(spacing=psize, data_names=[i],pixel_register=True)
+                grids[i] = vd.distance_mask(coordinates, maxdist=499, grid= grid)
+                tif_ = grids[i].rename(easting = 'x',northing='y')
+                tif_.rio.to_raster('/home/ggrl/Desktop/grids/3022/100m_25k/'+i+'_'+row.id_folha+'.tif')
