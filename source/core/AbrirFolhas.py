87069f88c25616322210ac1bc664a810c04b7fb0
ggrl <gabrielgoes@usp.br>
2024-02-07 04:37:49 -0300

    Classe AbrirFolhas está definida e testada. Precisa implementar testes mais robustos com pytest.

--- source/core/AbrirFolhas.py
+++ source/core/AbrirFolhas.py
@@ -22,11 +22,12 @@ class AbrirFolhas:
         '''
         try:
             self.file = setDB(gpkg)
-            print(f'--> Arquivo {gpkg} carregado com sucesso!')
-        except FileNotFoundError:
-            print(f'--> Arquivo {gpkg} não encontrado!')
+            print(f'--> Arquivo {self.file} carregado com sucesso!')
+        except Exception as e:
+            print(f'--> Erro ao carregar o arquivo! {e}')
+            print('--> Aqui deve ser seus geopackage.gpkg!')
 
-    # Método para importar a carta 1kk
+    # Método para importar a carta da escala escolhida
     def seleciona_escala(self, escala):
         '''
         Método responsável por importar as folhas de carta na escala escolhida.
@@ -36,31 +37,37 @@ class AbrirFolhas:
             Retorna:
                 carta: gdf - geodataframe com a carta escolhida
         '''
-        cartas = gpd.read_file(self.file, layer='fc_' + escala)
+        try:
+            # Lê o arquivo geopackage
+            self.cartas = gpd.read_file(self.file, layer='fc_' + escala)
 
-        return cartas
+            return self.cartas
+        except Exception as e:
+            print(f' --> Erro ao importar a carta! {e}')
+            print(' --> Escalas disponíveis: 25k, 50k, 100k, 250k, 500k e 1kk')
 
     # Define área de estudo
-    def define_area_de_estudo(cartas, id_folha_area_de_estudo):
+    def define_area_de_estudo(self, id_folha_area_de_estudo):
         '''
         Método responsável por definir a área de estudo.
         Recebe como parâmetros:
             cartas: gdf - geodataframe com as cartas
-            id_folha_area_de_estudo: str - id da folha de estudo
+            id_folha_area_de_estudo: str - id da folha da área de estudo
         Retorna:
-            folha_ade: gdf - geodataframe com a folha de estudo
+            folha_ade: gdf - geodataframe com a folha da área de estudo
         '''
-        folha_ade = cartas[cartas['id_folha'] == id_folha_area_de_estudo]
+        folha_ade = self.cartas[
+            self.cartas['id_folha'] == id_folha_area_de_estudo]
 
         return folha_ade
 
     # Criar bounding box para a folha escolhida
+    @staticmethod
     def cria_bbox(folha_ade):
         # Cria a bounding box
         minx, miny = folha_ade.bounds.minx, folha_ade.bounds.miny
         maxx, maxy = folha_ade.bounds.maxx, folha_ade.bounds.maxy
         bbox = (minx + 0.125, miny + 0.125, maxx - 0.125, maxy - 0.125)
-
         return bbox
 
     # Ler todas as folhas da carta escolhida contidas na id_folha_estudo
@@ -75,6 +82,8 @@ class AbrirFolhas:
         Retorna:
             folhas_estudo: gdf - geodataframe com as folhas da carta escolhida
         '''
+        # Cria a bounding box
+        bbox = self.cria_bbox(folha_ade)
         try:
             # Lê o arquivo geopackage
             folhas_area_de_estudo = gpd.read_file(self.file,
@@ -87,6 +96,7 @@ class AbrirFolhas:
             print(' --> Erro ao segmentar a área de estudo!')
 
     # Filtrar folhas de da área de estudo
+    @staticmethod
     def filtrar_folhas_estudo(folhas_area_de_estudo, id_folhas):
         '''
         Método responsável por filtrar as folhas da área de estudo.
@@ -96,4 +106,22 @@ class AbrirFolhas:
         '''
         # Filtra macro_gdf pr id_folha
         return folhas_area_de_estudo[folhas_area_de_estudo[
-            'id_folha'].isin(id_folhas)]
+            'id_folha'].str.contains(id_folhas)]
+
+
+# ------------------------------ EXECUÇÃO ------------------------------------
+if __name__ == '__main__':
+    # Teste da classe AbrirFolhas
+    # Cria um objeto da classe AbrirFolhas
+    folhas = AbrirFolhas()
+    # Importa a carta da escala 1:1.000.000
+    carta_1kk = folhas.seleciona_escala('1kk')
+    # Define a folha da área de estudo
+    SF23 = folhas.define_area_de_estudo('SF23')
+    # Segmenta a área de estudo na escala desejada
+    folhas_25k_SF23 = folhas.segmenta_area_de_estudo(SF23, '25k')
+    # Filtra as folhas da área de estudo
+    folhas_25k_SF23_YA = folhas.filtrar_folhas_estudo(folhas_25k_SF23,
+                                                      'SF23_YA')
+    print(f' -> número de folhas: {folhas_25k_SF23_YA.size}')
+    print(folhas_25k_SF23_YA.head())
