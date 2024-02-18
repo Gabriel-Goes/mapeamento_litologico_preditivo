2665b69a99538a8f1479c481f42b264ac5313b46
ggrl <gabrielgoes@usp.br>
2024-02-17 23:53:12 -0300

    argh

--- aerogeophysics_data_analysis.py
+++ aerogeophysics_data_analysis.py
@@ -1,27 +1,30 @@
 import pandas as pd
-import geopandas as gpd
+
 
 def set_gdb(filepath=None):
     gdb = '/home/ggrl/geodatabase/' + filepath
 
     return gdb
 
-def import_xyz(filename,rowskiped):
-    data_cols = 'KB DATA BARO UB THB COSMICO CTB UUP ALTURA KPERC eU eTH CTEXP UTHRAZAO X Y UKRAZAO MDT THKRAZAO LIVE_TIME CTCOR KCOR THCOR UCOR HORA GPSALT LATITUDE FIDUCIAL TEMP LONGITUDE'.split(" ")
 
-    geof = pd.read_csv(set_gdb('xyz/'+ filename),
-                         names = data_cols,
-                         delim_whitespace = True,
-                         skiprows = rowskiped, # Linhas de cabeçalho
-                         usecols = ["X","Y","LATITUDE","LONGITUDE",
-                                   "KPERC","eU","eTH","CTCOR",
-                                   "THKRAZAO","UTHRAZAO","UKRAZAO","MDT"])
+def import_xyz(filename, rowskiped):
+    data_cols = 'KB DATA BARO UB THB COSMICO CTB UUP ALTURA KPERC eU eTH CTEXP\
+    UTHRAZAO X Y UKRAZAO MDT THKRAZAO LIVE_TIME CTCOR KCOR THCOR UCOR HORA\
+    GPSALT LATITUDE FIDUCIAL TEMP LONGITUDE'.split(" ")
+
+    geof = pd.read_csv(set_gdb('xyz/' + filename),
+                       names=data_cols,
+                       delim_whitespace=True,
+                       skiprows=rowskiped,
+                       usecols=["X", "Y", "LATITUDE", "LONGITUDE",
+                                "KPERC", "eU", "eTH", "CTCOR",
+                                "THKRAZAO", "UTHRAZAO", "UKRAZAO", "MDT"])
 
     return geof
 
 
 # Dado Aerogeofísico
-geof = import_xyz('1105_GamaLine.XYZ',10)
+geof = import_xyz('1105_GamaLine.XYZ', 10)
 geof.dropna(inplace=True)
 
 print(geof.head(10))
@@ -31,9 +34,9 @@ print(geof.info())
 def list_columns(geof):
     print('Listando atributos dos dados geofisicos')
     atributos_geof = list(geof.columns)             # DataFrame.columns
-    lista_atributo_geof=[]
-    lista_atributo_geog=[]
-    lista_atributo_proj=[]
+    lista_atributo_geof = []
+    lista_atributo_geog = []
+    lista_atributo_proj = []
 
     for atributo in atributos_geof:
         if atributo == 'LATITUDE':
@@ -43,7 +46,7 @@ def list_columns(geof):
         elif atributo == 'LONG':
             lista_atributo_geog.append(atributo)
         elif atributo == 'LAT':
-            lista_atributo_geog.append(atributo) 
+            lista_atributo_geog.append(atributo)
         elif atributo == 'X':
             lista_atributo_proj.append(atributo)
         elif atributo == 'Y':
@@ -58,10 +61,10 @@ def list_columns(geof):
             lista_atributo_proj.append(atributo)
         else:
             lista_atributo_geof.append(atributo)
-    
-    codigo=str(geof)        
-    
-    print(f"# --- # Listagem de dados do aerolevantamento:  ")
+
+    # codigo =str(geof)
+
+    print("# --- # Listagem de dados do aerolevantamento:  ")
     print(f"Lista de atributos geofísicos = {lista_atributo_geof}")
     print(f"lista de atributos geograficos = {lista_atributo_geog}")
     print(f"lista de atributos projetados = {lista_atributo_proj}")
@@ -69,11 +72,4 @@ def list_columns(geof):
 
 
 meta_listas = list_columns(geof)
-
-
-
-
-
-
-
-dicionario = {'':''}
+dicionario = {'': ''}
